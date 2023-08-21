0e97bf919de710ad7b4b7139af141dc6cd479a65
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2023-08-21 08:45:58 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -33,6 +33,7 @@ cmake --install .
 sed -i.bak -e s,"/home/conda/feedstock_root/build_artifacts/kokkos_[0-9]*/_build_env/bin/","",g $PREFIX/bin/kokkos_launch_compiler
 rm $PREFIX/bin/*.bak
 
+if [ ! -z ${cuda_compiler_version+x} ]; then
 sed -i.bak '/INCLUDE(CMakeFindDependencyMacro)/a\
 \
 \IF(NOT TARGET CUDA::cudart)\
@@ -41,6 +42,7 @@ sed -i.bak '/INCLUDE(CMakeFindDependencyMacro)/a\
 \IF(NOT TARGET CUDA::cuda_driver)\
 \  MESSAGE(SEND_ERROR "The CUDA::cuda_driver target was not found; use find_package(CUDAToolkit REQUIRED) before find_package(Kokkos).")\
 \ENDIF()' $PREFIX/lib/cmake/Kokkos/KokkosConfig.cmake
+fi
 
 if [[ "$cuda_compiler_version" == "11."* ]]; then
 sed -i.bak -e s,"/home/conda/feedstock_root/build_artifacts/kokkos_[0-9]*/_build_env/x86_64-conda-linux-gnu/sysroot/lib/libcuda.so","/usr/lib64/libcuda.so",g $PREFIX/lib/cmake/Kokkos/KokkosConfig.cmake
